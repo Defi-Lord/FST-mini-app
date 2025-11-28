6a37dd62e6de1e5d1dade23ea58e27c67c503ab7
Defi-Lord <dammypeck@gmail.com>
2025-11-28 16:18:36 +0100

    fixing api.ts errors since ages

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,2 @@
-const API = (import.meta as any).env?.VITE_API_BASE ?? 'http://localhost:4000';
-
-export async function api<T>(path: string, init: RequestInit = {}): Promise<T> {
-  const token = localStorage.getItem('auth_token');
-  const headers = new Headers(init.headers);
-  if (token) headers.set('Authorization', `Bearer ${token}`);
-  const res = await fetch(`${API}${path}`, { ...init, headers, credentials: 'include' });
-  if (!res.ok) throw new Error(await res.text());
-  return res.json() as Promise<T>;
-}
+// DO NOT USE. API MOVED TO /src/api.ts
+export * from "../api"
